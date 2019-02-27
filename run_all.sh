853b1c10baad24a1945ce9d72c96f421590e1df5
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2019-02-27 11:34:52 +0000

    update script
    * run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 inputs=( me_at_the_zoo trending_today videos_worth_spreading kittens )
 for file in "${inputs[@]}"; do
-echo "Running $file"
-./run.sh $file
+    echo "Running $file"
+    ./run.sh $file
 done
